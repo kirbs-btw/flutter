551adb1f95d2ae0df8e24d110ef855630f63f1d7
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-05-06 17:41:02 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Hind',
         primarySwatch: Colors.teal,
       ),
       home: MyHomePage(title: 'Hello Sir wittu!'),
@@ -45,11 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -70,8 +66,25 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Hello Sir',
+            const Text(
+                "Hello",
+              style: TextStyle(
+                  fontSize: 75,
+                  fontFamily: 'GothamBlack',
+              ),
+
+            ),
+            const Text(
+              "Bastian Lipka",
+              style: TextStyle(
+                fontSize: 25,
+                fontFamily: 'GothamHTF',
+              ),
+            ),
+            const OutlineButton(
+                child: const Text('Good morning Sir'),
+                onPressed: null,
+
             ),
           ],
         ),
