b10a8a286370fe64218b7daa3f8fb491f9e35b94
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-05-07 13:00:15 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -39,31 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -81,10 +60,37 @@ class _MyHomePageState extends State<MyHomePage> {
                 fontFamily: 'GothamHTF',
               ),
             ),
-            const OutlineButton(
-                child: const Text('Good morning Sir'),
-                onPressed: null,
-
+            Container(
+                margin: new EdgeInsets.symmetric(vertical: 150.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        RaisedButton(
+                          child: const Text('Project 1'),
+                          onPressed: null,
+                        ),
+                        RaisedButton(
+                          child: const Text('Project 2'),
+                          onPressed: null,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        RaisedButton(
+                          child: const Text('Project 3'),
+                          onPressed: null,
+                        ),
+                        RaisedButton(
+                          child: const Text('Project 4'),
+                          onPressed: null,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
             ),
           ],
         ),
