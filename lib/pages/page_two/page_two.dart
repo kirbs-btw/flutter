eb736e4e8dd0c34ca49a01dbd71891d56303f3f4
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-05-15 09:37:17 +0200

    Add files via upload

--- lib/pages/page_two/page_two.dart
+++ lib/pages/page_two/page_two.dart
@@ -22,41 +22,58 @@ class _PageTwo extends State<PageTwo> {
           // color: Color(int.parse("0xfffff4e2")),
           //margin: EdgeInsets.fromLTRB(100, 150, 100, 150),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  const Text(
-                    'Page 2',
-                    style: TextStyle(
-                      fontFamily: 'GothamBlack',
-                      fontSize: 50,
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 100, 0, 50),
+                    child:  const Text(
+                      'Page 2',
+                      style: TextStyle(
+                        fontFamily: 'GothamBlack',
+                        fontSize: 50,
+                      ),
                     ),
                   ),
                   new Container(
-                    padding: EdgeInsets.fromLTRB(1, 10, 1, 10),
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    height: MediaQuery.of(context).size.height * 0.2,
                     decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(15.0),
+                     borderRadius: new BorderRadius.circular(15.0),
                       color: Color(int.parse("0xfffec673")),
                     ),
                   ),
-                  Text(
-                    "Stuff",
-                    style: TextStyle(
-                      fontFamily: '',
-                      fontSize: 10,
-                    ),
+                  Row(
+                    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(25,50,0,10),
+                        child: Text(
+                          "Your life",
+                          style: TextStyle(
+                            fontFamily: '',
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       new Container(
-                        padding: EdgeInsets.fromLTRB(90, 90, 90, 90),
+                        height: MediaQuery.of(context).size.height * 0.225,
+                        width: MediaQuery.of(context).size.height * 0.225,
+                        //padding: EdgeInsets.fromLTRB(90, 90, 90, 90),
                         decoration: new BoxDecoration(
                           borderRadius: new BorderRadius.circular(15.0),
                           color: Color(int.parse("0xff222222")),
                         ),
                       ),
                       new Container(
-                        padding: EdgeInsets.fromLTRB(90, 90, 90, 90),
+                        height: MediaQuery.of(context).size.height * 0.225,
+                        width: MediaQuery.of(context).size.height * 0.225,
+                        //padding: EdgeInsets.fromLTRB(90, 90, 90, 90),
                         decoration: new BoxDecoration(
                           borderRadius: new BorderRadius.circular(15.0),
                           color: Color(int.parse("0xfffec673")),
